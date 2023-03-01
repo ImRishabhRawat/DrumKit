be3e886270f7894739dbec7973a3416703e9ca79
ImRishabhRawat <104312137+ImRishabhRawat@users.noreply.github.com>
2023-03-01 23:10:18 +0530

    commit

--- index.js
+++ index.js
@@ -56,3 +56,11 @@ function makeSound(key) {
 
     };
 }
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+ currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    },100);
+}
